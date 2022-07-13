57991008f63da8f7bf2e290a0f31e8cbd05cff26
aimdexter <draimade@gmail.com>
2022-07-13 21:54:47 +0100

    digital clock done

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const clock = document.querySelector('.clock');
+const ho = document.querySelector('.hours');
+const min = document.querySelector('.minutes');
+const sec = document.querySelector('.seconds');
+
 
 const tick = () => {
     const now = new Date();
     const hours = now.getHours();
     const minutes = now.getMinutes();
     const seconds = now.getSeconds();
-    const html = `
-        <span>${hours}</span> :
-        <span>${minutes}</span> :
-        <span>${seconds}</span>
-    `;
+    const h = `${("0" + hours).slice(-2)}<span> :</span>`;
+    const m = `${("0" + minutes).slice(-2)}<span> :</span>`;
+    const s = `${("0" + seconds).slice(-2)}`;
 
-    clock.innerHTML = html; 
+    ho.innerHTML = h;
+    min.innerHTML = m;
+    sec.innerHTML = s;
 };
 
 setInterval(tick, 1000);
